41aedf4a25ebb9aa69abfe4afff3a16572f43f8c
meng_lu <bouncy.lu@hotmail.com>
2024-04-27 22:02:59 -0700

    Finish encoder

--- PA2_code/main.py
+++ PA2_code/main.py
@@ -5,6 +5,7 @@ import os
 
 from tokenizer import SimpleTokenizer
 from dataset import SpeechesClassificationDataset, LanguageModelingDataset
+from transformer import Classifier
 
 
 seed = 42
@@ -20,49 +21,53 @@ n_embd = 64  # Embedding dimension
 n_head = 2  # Number of attention heads
 n_layer = 4  # Number of transformer layers
 
-
 eval_interval = 100  # How often to evaluate train and test perplexity during training
-max_iters = 500 # For language modeling, we can process all the batches for the entire dataset, but that takes a while, so we'll limit it to 500 iterations. For batch size of 16 and block size of  32, this is roughly, this is  500 * 16 * 32 = 256000 tokens, SOTA LMs are trained on trillions of tokens, so this is a very small dataset.
+max_iters = 500  # For language modeling, we can process all the batches for the entire dataset, but that takes a while, so we'll limit it to 500 iterations. For batch size of 16 and block size of  32, this is roughly, this is  500 * 16 * 32 = 256000 tokens, SOTA LMs are trained on trillions of tokens, so this is a very small dataset.
 eval_iters = 200  # Number of iterations to evaluate perplexity on the test set
 
-
-## classifier training hyperparameters. It is a simple 1 hidden layer feedforward network, with input 
+## classifier training hyperparameters. It is a simple 1 hidden layer feedforward network, with input
 ## size of 64, hidden size of 50 and output size of 3.
 
 n_input = 64  # Input size for the classifier, should match the embedding size of the transformer
 n_hidden = 100  # Hidden size for the classifier
 n_output = 3  # Output size for the classifier, we have 3 classes
-epochs_CLS = 15 # epochs for classifier training
+epochs_CLS = 15  # epochs for classifier training
+
 
 def load_texts(directory):
     """
-    This function loads all texts from the specified directory, ignoring any files with "test" in their name. The text is used for "training" the tokenizer. Since our tokenizer is simple, we don't need to do any training, but we still need to ignore the test data. 
+    This function loads all texts from the specified directory, ignoring any files with "test" in their name. The text is used for "training" the tokenizer. Since our tokenizer is simple, we don't need to do any training, but we still need to ignore the test data.
     """
 
     texts = []
     files = os.listdir(directory)
-    for filename in files: 
+    for filename in files:
         if "test" in filename:  ## don't "read test files"
             continue
-        with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
+        with open(os.path.join(directory, filename), "r", encoding="utf-8") as file:
             texts.append(file.read())
     return texts
 
 
-
 def collate_batch(batch):
-    """ Collate a batch of data into a single tensor with padding."""
+    """Collate a batch of data into a single tensor with padding."""
     data, labels = zip(*batch)  # Separate the data and labels
     # Pad sequences to the fixed length
     padded_sequences = pad_sequence(data, batch_first=True, padding_value=0)
     padded_sequences = padded_sequences[:, :block_size]  # Truncate if longer
     # Add padding if shorter
-    padded_sequences = torch.nn.functional.pad(padded_sequences, (0, max(0, block_size - padded_sequences.shape[1])), "constant", 0)
-    labels = torch.stack(labels)  
+    padded_sequences = torch.nn.functional.pad(
+        padded_sequences,
+        (0, max(0, block_size - padded_sequences.shape[1])),
+        "constant",
+        0,
+    )
+    labels = torch.stack(labels)
     return padded_sequences, labels
 
+
 def compute_classifier_accuracy(classifier, data_loader):
-    """ Compute the accuracy of the classifier on the data in data_loader."""
+    """Compute the accuracy of the classifier on the data in data_loader."""
     classifier.eval()
     total_correct = 0
     total_samples = 0
@@ -73,24 +78,26 @@ def compute_classifier_accuracy(classifier, data_loader):
             _, predicted = torch.max(outputs.data, 1)
             total_correct += (predicted == Y).sum().item()
             total_samples += Y.size(0)
-        accuracy = (100 * total_correct / total_samples)
+        accuracy = 100 * total_correct / total_samples
         classifier.train()
         return accuracy
 
 
 def compute_perplexity(decoderLMmodel, data_loader, eval_iters=100):
-    """ Compute the perplexity of the decoderLMmodel on the data in data_loader.
+    """Compute the perplexity of the decoderLMmodel on the data in data_loader.
     Make sure to use the cross entropy loss for the decoderLMmodel.
     """
     decoderLMmodel.eval()
-    losses= []
+    losses = []
     for X, Y in data_loader:
         X, Y = X.to(device), Y.to(device)
-        loss = decoderLMmodel(X, Y) # your model should be computing the cross entropy loss
+        loss = decoderLMmodel(
+            X, Y
+        )  # your model should be computing the cross entropy loss
         losses.append(loss.item())
         total_loss += loss.item()
-        if len(losses) >= eval_iters: break
-
+        if len(losses) >= eval_iters:
+            break
 
     losses = torch.tensor(losses)
     mean_loss = losses.mean()
@@ -99,29 +106,47 @@ def compute_perplexity(decoderLMmodel, data_loader, eval_iters=100):
     decoderLMmodel.train()
     return perplexity
 
-def main():
 
+def main():
     print("Loading data and creating tokenizer ...")
-    texts = load_texts('speechesdataset')
-    tokenizer = SimpleTokenizer(' '.join(texts)) # create a tokenizer from the data
+    texts = load_texts("speechesdataset")
+    tokenizer = SimpleTokenizer(" ".join(texts))  # create a tokenizer from the data
     print("Vocabulary size is", tokenizer.vocab_size)
 
-    train_CLS_dataset = SpeechesClassificationDataset(tokenizer, "speechesdataset/train_CLS.tsv")
-    train_CLS_loader = DataLoader(train_CLS_dataset, batch_size=batch_size,collate_fn=collate_batch,shuffle=True)
+    train_CLS_dataset = SpeechesClassificationDataset(
+        tokenizer, "speechesdataset/train_CLS.tsv"
+    )
+    train_CLS_loader = DataLoader(
+        train_CLS_dataset, batch_size=batch_size, collate_fn=collate_batch, shuffle=True
+    )
 
-  
     inputfile = "speechesdataset/train_LM.txt"
-    with open(inputfile, 'r', encoding='utf-8') as f:
+    with open(inputfile, "r", encoding="utf-8") as f:
         lmtrainText = f.read()
-    train_LM_dataset = LanguageModelingDataset(tokenizer, lmtrainText,  block_size)
+    train_LM_dataset = LanguageModelingDataset(tokenizer, lmtrainText, block_size)
     train_LM_loader = DataLoader(train_LM_dataset, batch_size=batch_size, shuffle=True)
 
-     # for the classification  task, you will train for a fixed number of epochs like this:
+    # for the classification  task, you will train for a fixed number of epochs like this:
+    CLS_model = Classifier(
+        tokenizer.vocab_size,
+        n_embd,
+        n_head,
+        n_layer,
+        block_size,
+        0.1,
+        n_input,
+        n_hidden,
+        n_output,
+    ).to(device)
+    optimizer = torch.optim.AdamW(CLS_model.parameters(), lr=learning_rate)
     for epoch in range(epochs_CLS):
         for xb, yb in train_CLS_loader:
             xb, yb = xb.to(device), yb.to(device)
-            # CLS training code here
-
+            logits, loss = CLS_model(xb, yb)
+            optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            optimizer.step()
+        print(epoch, loss.item())
 
     # for the language modeling task, you will iterate over the training data for a fixed number of iterations like this:
     for i, (xb, yb) in enumerate(train_LM_loader):
@@ -129,9 +154,6 @@ def main():
             break
         xb, yb = xb.to(device), yb.to(device)
         # LM training code here
-
-    
-
 
 
 if __name__ == "__main__":
--- PA2_code/transformer.py
+++ PA2_code/transformer.py
@@ -1,1 +1,164 @@
-# add all  your Encoder and Decoder code here
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Head(nn.Module):
+    def __init__(
+        self, n_embd: int, head_size: int, block_size: int, dropout: float, masked: bool
+    ):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
+        self.masked = masked
+
+    def forward(self, x: torch.Tensor):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        wei = q @ k.transpose(-2, -1) * k.shape[-1] ** -0.5
+        if self.masked:
+            wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+        v = self.value(x)
+        out = wei @ v
+        return out
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(
+        self,
+        n_embd: int,
+        num_heads: int,
+        head_size: int,
+        block_size: int,
+        dropout: float,
+        masked: bool,
+    ):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [
+                Head(n_embd, head_size, block_size, dropout, masked)
+                for _ in range(num_heads)
+            ]
+        )
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+
+class FeedFoward(nn.Module):
+    def __init__(self, n_embd: int, dropout: float):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    def __init__(
+        self, n_embd: int, n_head: int, block_size: int, dropout: float, masked: bool
+    ):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(
+            n_embd, n_head, head_size, block_size, dropout, masked
+        )
+        self.ffwd = FeedFoward(n_embd, dropout)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x: torch.Tensor):
+        x = x + self.ln1(self.sa(x))
+        x = x + self.ln2(self.ffwd(x))
+        return x
+
+
+class Encoder(nn.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        n_embd: int,
+        n_head: int,
+        n_layer: int,
+        block_size: int,
+        dropout: float,
+    ):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(
+            *[
+                Block(n_embd, n_head, block_size, dropout, masked=False)
+                for _ in range(n_layer)
+            ]
+        )
+        self.ln_f = nn.LayerNorm(n_embd)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx: torch.Tensor):
+        B, T = idx.shape
+        tok_emb = self.token_embedding_table(idx)
+        pos_emb = self.position_embedding_table(torch.arange(T))
+        x = tok_emb + pos_emb
+        x = self.blocks(x)
+        x = self.ln_f(x)
+        return x
+
+
+class Classifier(nn.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        n_embd: int,
+        n_head: int,
+        n_layer: int,
+        block_size: int,
+        dropout: float,
+        n_input: int,
+        n_hidden: int,
+        n_output: int,
+    ):
+        super().__init__()
+        assert n_embd == n_input
+        self.encoder = Encoder(
+            vocab_size, n_embd, n_head, n_layer, block_size, dropout
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(n_input, n_hidden),
+            nn.ReLU(),
+            nn.Linear(n_hidden, n_output),
+        )
+
+    def forward(self, x: torch.Tensor, targets: torch.Tensor = None):
+        x = self.encoder(x)
+        x = x.mean(dim=1)
+        logits = self.classifier(x)
+
+        if targets is None:
+            loss = None
+        else:
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
